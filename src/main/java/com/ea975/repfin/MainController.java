cbe6fd47cb6277469b178d766cc0d4d6705411d0
thiagoitagaki <thiago.itagaki@gmail.com>
2016-06-13 00:32:38 -0300

    Bootstraping application

---
+++ src/main/java/com/ea975/repfin/MainController.java
@@ -0,0 +1,17 @@
+package com.ea975.repfin;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Map;
+
+@Controller
+public class MainController {
+
+    @RequestMapping("/")
+    public String index(Map<String, Object> model){
+        model.put("message", "Hello World Lyra");
+        return "index";
+    }
+
+}
