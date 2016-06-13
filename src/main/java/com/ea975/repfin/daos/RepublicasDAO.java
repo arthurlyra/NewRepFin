4109e6d0b105512dd31b512f2a43694fbde9a707
Arthur Lyra <arthurlyra2502@gmail.com>
2016-06-13 15:49:07 -0300

    Adicionando DAO's - Lyra

---
+++ src/main/java/com/ea975/repfin/daos/RepublicasDAO.java
@@ -0,0 +1,4 @@
+package com.ea975.repfin.daos;
+
+public class RepublicasDAO {
+}
---
+++ src/main/java/com/ea975/repfin/daos/TransactionsDAO.java
@@ -0,0 +1,6 @@
+package com.ea975.repfin.daos;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface TransactionsDAO extends CrudRepository<Transactions, Integer> {
+}
